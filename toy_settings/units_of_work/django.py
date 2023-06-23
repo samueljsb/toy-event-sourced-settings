08f45181973033cde35609b2ecb5a13a62d8a007
Samuel Searles-Bryant <sam.searles-bryant@krakentechnologies.ltd>
2023-06-23 18:55:28 +0100

    Create concrete committers
    Co-authored-by: Charlie Denton <charlie@meshy.co.uk>

--- toy_settings/units_of_work/django.py
+++ toy_settings/units_of_work/django.py
@@ -1,13 +1,34 @@
 from __future__ import annotations
 
+from contextlib import contextmanager
 from typing import Any
+from typing import Iterator
 
 import attrs
 from django.db import transaction
 from typing_extensions import Self
 
 from toy_settings.application import unit_of_work
+from toy_settings.domain import events
+from toy_settings.repositories.django import models
 from toy_settings.repositories.django import repo
+
+
+class DjangoCommitter(unit_of_work.Committer):
+    @contextmanager
+    def atomic(self) -> Iterator[None]:
+        with transaction.atomic():
+            yield
+
+    def handle(self, event: events.Event) -> None:
+        event_type, event_type_version = models.EVENT_TYPES[type(event)]
+        models.Event.objects.create(
+            event_type=event_type,
+            event_type_version=event_type_version,
+            timestamp=event.timestamp,
+            key=event.key,
+            payload=models.Event.payload_converter.dumps(event),
+        )
 
 
 @attrs.define
