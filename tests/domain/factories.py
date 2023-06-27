6270aa56f13056423f20d8f8129f9e19f5d29019
Samuel Searles-Bryant <sam.searles-bryant@krakentechnologies.ltd>
2023-06-27 17:10:55 +0100

    Move event attributes so we don't make assumptions

--- tests/domain/factories.py
+++ tests/domain/factories.py
@@ -13,7 +13,6 @@ class Event(factory.Factory):
         abstract = True
 
     timestamp = factory.LazyFunction(datetime.datetime.now)
-    by = "me"
 
 
 class Set(Event):
@@ -23,6 +22,8 @@ class Set(Event):
     key: str
     value: str
 
+    by = "me"
+
 
 class Changed(Event):
     class Meta:
@@ -31,9 +32,13 @@ class Changed(Event):
     key: str
     new_value: str
 
+    by = "me"
+
 
 class Unset(Event):
     class Meta:
         model = events.Unset
 
     key: str
+
+    by = "me"
--- toy_settings/domain/events.py
+++ toy_settings/domain/events.py
@@ -9,7 +9,6 @@ import attrs
 @attrs.frozen
 class Event(abc.ABC):
     timestamp: datetime.datetime
-    by: str
 
     key: str
 
@@ -18,12 +17,16 @@ class Event(abc.ABC):
 class Set(Event):
     value: str
 
+    by: str
+
 
 @attrs.frozen
 class Changed(Event):
     new_value: str
 
+    by: str
+
 
 @attrs.frozen
 class Unset(Event):
-    pass
+    by: str
