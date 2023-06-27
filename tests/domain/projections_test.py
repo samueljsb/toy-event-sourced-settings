92b1c56b6e8cf4a2070f469a3b62f5da0050aed7
Samuel Searles-Bryant <sam.searles-bryant@krakentechnologies.ltd>
2023-06-27 17:20:43 +0100

    Use factories in projections tests

--- tests/domain/projections_test.py
+++ tests/domain/projections_test.py
@@ -1,33 +1,17 @@
 from __future__ import annotations
 
-import datetime
-
-from toy_settings.domain import events
+from testing.domain import factories
 from toy_settings.domain import projections
-
-
-def _set_event(key: str, value: str) -> events.Set:
-    return events.Set(timestamp=datetime.datetime.now(), by="me", key=key, value=value)
-
-
-def _changed_event(key: str, new_value: str) -> events.Changed:
-    return events.Changed(
-        timestamp=datetime.datetime.now(), by="me", key=key, new_value=new_value
-    )
-
-
-def _unset_event(key: str) -> events.Unset:
-    return events.Unset(timestamp=datetime.datetime.now(), by="me", key=key)
 
 
 def test_current_settings():
     history = [
-        _set_event("set-once", "42"),
-        _set_event("set-and-changed", "42"),
-        _changed_event("set-and-changed", "43"),
-        _set_event("set-and-unset", "42"),
-        _unset_event("set-and-unset"),
-        _unset_event("never-set"),  # handles unexpected key
+        factories.Set(key="set-once", value="42"),
+        factories.Set(key="set-and-changed", value="42"),
+        factories.Changed(key="set-and-changed", new_value="43"),
+        factories.Set(key="set-and-unset", value="42"),
+        factories.Unset(key="set-and-unset"),
+        factories.Unset(key="never-set"),  # handles unexpected key
     ]
 
     settings = projections.current_settings(history)
